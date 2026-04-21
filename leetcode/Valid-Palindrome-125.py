fbab6ed30ac9e4b6dee24033279feb31354d5132
devsk18 <kthampansam18@gmail.com>
2026-04-21 21:45:28 +0530

    LC-125: solved (Valid Palindrome)

---
+++ leetcode/Valid-Palindrome-125.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        l,r = 0,n-1
+
+        while l<r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+# TC : O(N)
+# SC : O(1)
+
+# Suggested: Two Pointers/Simulation
+# Key Idea: Using two pointers to validate palindromes in-place while skipping non-alphanumeric characters.
