66f85486843dc8ed14d6c8a5cc36ab1f1492a0a4
devsk18 <kthampansam18@gmail.com>
2026-04-29 23:17:23 +0530

    LC-206: solved (Reverse Linked List)

---
+++ leetcode/Reverse-Linked-List-206.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        prev = None
+        
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        
+        return prev
+
+
+# TC : O(N)
+# SC : O(1)
+
+# Suggested: Linked List/Recursion
+# Key Idea: Reversing a singly linked list by iteratively updating node pointers using two references.
