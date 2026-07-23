76921be6a6557f05dd446f49177797f56a8a7dad
devsk18 <kthampansam18@gmail.com>
2026-07-23 23:18:18 +0530

    LC-1143: solved (Longest Common Subsequence)

---
+++ leetcode/Longest-Common-Subsequence-1143.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m,n = len(text1), len(text2)
+
+        @cache
+        def longest(i, j):
+            if i == m or j == n:
+                return 0
+            elif text1[i] == text2[j]:
+                return 1 + longest(i+1, j+1)
+            else:
+                return max(longest(i+1, j), longest(i, j+1))
+        
+        return longest(0, 0)
+    
+# TC : O(m*n)
+# SC : O(m*n)
