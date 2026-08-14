f48fd010bda464f8383faeffb47fc8fba885ed28
devsk18 <kthampansam18@gmail.com>
2026-08-14 22:48:00 +0530

    LC-177: solved (Nth Highest Salary)

---
+++ leetcode/Nth-Highest-Salary-177.py
@@ -0,0 +1,13 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    # Write your MySQL query statement below.
+    SET N = N - 1;
+    RETURN (
+        SELECT DISTINCT salary 
+        FROM Employee 
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END
+
+
